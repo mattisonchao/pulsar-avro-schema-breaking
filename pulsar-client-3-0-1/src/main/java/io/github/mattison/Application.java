ccddf02811723f983ce088f1e45a85befd966011
mattisonchao <mattisonchao@gmail.com>
2026-01-04 11:09:10 +0800

    feat: add different client module

---
+++ pulsar-client-3-0-1/src/main/java/io/github/mattison/Application.java
@@ -0,0 +1,21 @@
+package io.github.mattison;
+
+import org.apache.pulsar.client.api.Producer;
+import org.apache.pulsar.client.api.PulsarClient;
+import org.apache.pulsar.client.api.PulsarClientException;
+import org.apache.pulsar.client.api.Schema;
+
+public class Application {
+
+    public static void main(String[] args) throws PulsarClientException {
+        final PulsarClient client = PulsarClient.builder()
+                .serviceUrl("pulsar://localhost:6650")
+                .build();
+
+        final Producer<DataRecordOuterClass.DataRecord> producer =
+                client.newProducer(Schema.PROTOBUF(DataRecordOuterClass.DataRecord.class))
+                        .topic("test")
+                        .create();
+        client.close();
+    }
+}
